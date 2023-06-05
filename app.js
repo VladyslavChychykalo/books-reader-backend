3337fd539be44aa19ccd433956f3a5a7af709e77
Vladyslav <vladyslav@MacBook-Air-Vladyslav.local>
2023-06-05 22:34:19 +0300

    update

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
 
-const booksRouter = require("./routers/api/books");
+const contactsRouter = require('./routes/api/contacts')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-// есть ли в запросе body, смотрит какой тип Content Type
-// если application.json, то с помощью JSON.parse превразает в объект
-app.use(express.json());
-app.use("/api/books", booksRouter);
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-// обрабка не вірного запроса
-app.use(() => {
-  res.status(404).json({ message: "Not found" });
-});
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
 
-// next(error)
+app.use('/api/contacts', contactsRouter)
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' })
+})
+
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
-});
+  res.status(500).json({ message: err.message })
+})
 
 module.exports = app
--- middlewars/validateBody.js
+++ middlewars/validateBody.js
@@ -1,16 +1,11 @@
 const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
-  const func = (req, res, next) => {
+  const func = (req, _, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      // вызов next прерывает функцию как return
-      // тут нет контекста try catch, поэтому throw не используем
-      // вызывается обработчик ошибок
       next(HttpError(400, error.message));
     }
-
-    // переход к следующей middleware или к финальной функции
     next();
   };
 
